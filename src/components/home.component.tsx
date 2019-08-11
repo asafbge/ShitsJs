ba20f44bafbae40c8a826f141828076aaf311b14
Asaf Benjaminov <asaf.benjaminov@gameffective.com>
2019-08-11 16:29:36 +0300

    home component

---
+++ src/components/home.component.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Hello!
+            </div>
+        );
+    }
+}
